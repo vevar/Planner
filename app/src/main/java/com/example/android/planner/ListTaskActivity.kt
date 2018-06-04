5035ee3843946fea2eb06943ce1e60764768b0c6
vevar <webvevar@gmail.com>
2018-06-04 19:33:31 +0700

    New

---
+++ app/src/main/java/com/example/android/planner/ListTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.planner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_task)
+    }
+}
