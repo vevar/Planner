6d8442a9f3f44b80784cc88054d91072e904df38
vevar <webvevar@gmail.com>
2018-06-09 22:57:10 +0700

    Done touch of relativeLayout at ListTaskActivity

--- app/src/main/java/com/example/android/planner/ListTaskActivity.kt
+++ app/src/main/java/com/example/android/planner/ListTaskActivity.kt
@@ -1,35 +1,44 @@
 package com.example.android.planner
 
+import android.annotation.SuppressLint
 import android.app.FragmentManager
 import android.app.FragmentTransaction
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.RelativeLayout
+import android.widget.Toast
 
 
 class ListTaskActivity : AppCompatActivity() {
-    //TODO Add fragments BlockTimeFragment
+    //TODO What is it?
+    //TODO Make add task to RelativeLayout
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         createTimeList()
-
         setContentView(R.layout.activity_list_task)
+        val relativeLayout = findViewById<RelativeLayout>(R.id.forTimeTask)
+        relativeLayout.setOnTouchListener { v, m ->
+            Toast.makeText(applicationContext, " X:${m.x} Y: ${m.y}",
+                    Toast.LENGTH_SHORT).show()
+            true
+        }
+
     }
 
-    private fun createTimeList(){
-        val fragManager : FragmentManager = fragmentManager
-        val transaction : FragmentTransaction = fragManager.beginTransaction()
-        val strMinutes : String = ":00"
-        var time : String
-        for (i in 0..23){
-            if (i < 10){
-                time = "0$i:00"
-            }
-            else{
-                time = "$i:00"
-            }
-            transaction.add(R.id.day_markup, BlockTimeFragment(time))
+    private fun createTimeList() {
+        val fragManager: FragmentManager = fragmentManager
+        val transaction: FragmentTransaction = fragManager.beginTransaction()
+
+        for (i in 0..9) {
+            transaction.add(R.id.day_markup, BlockTimeFragment("0$i:00"))
         }
+
+        for (i in 10..23) {
+            transaction.add(R.id.day_markup, BlockTimeFragment("$i:00"))
+        }
+
         transaction.commit()
     }
 
